6f15417d221d424af750fd3ffc7436019bbddfaa
ashiqur.superfly <ashiqur.buet16@gmail.com>
2020-07-01 19:24:40 +0600

    refactor ui

--- app/src/main/java/com/ashiqurrahman/daterangepickerdialog/MainActivity.kt
+++ app/src/main/java/com/ashiqurrahman/daterangepickerdialog/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val d = PickTimeRangeDialog()
+        d.isCancelable = false
         d.show(supportFragmentManager, "")
     }
 }
